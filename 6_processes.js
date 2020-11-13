3872e82d5f7f8d07f7426b4960d096b4844d94c9
cd_trich <31612691+cdtrich@users.noreply.github.com>
2020-11-13 18:33:05 +0100

    min

--- 6_processes.js
+++ 6_processes.js
@@ -1,24 +1,8 @@
-///////////////////////////////////////////////////////////////////////////
-//////////////////////////// libs /////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-
-// import * as d3 from "d3";
-// import { easeBounceIn, easeCubic, easeExp, easePoly } from "d3";
-// import { csv } from "d3-fetch";
-// import _ from "lodash";
-
-// import mustache
-// https://github.com/janl/mustache.js
-// const Mustache = require("mustache");
-
 console.clear();
 
-///////////////////////////////////////////////////////////////////////////
 //////////////////////////// drawing function /////////////////////////////
-///////////////////////////////////////////////////////////////////////////
 
 const createChart = async () => {
-	// const url = "https://eucyberdirect.eu/wp-content/uploads/2020/11/cpi_cyber_operations_database_2020_version-1.0.csv";
 	const url = "./data/CPI_Cyber_Operations_Database_2020_Version 1.0.csv";
 
 	//////////////////////////// data /////////////////////////////////////////
@@ -128,8 +112,6 @@ const createChart = async () => {
 	const update = () => {
 		//////////////////////////// sizes ///////////////////////////////////
 		const el = document.getElementById("col");
-		// const size = 795;
-		// const size = d3.min([window.innerWidth * 0.99, window.innerHeight * 0.99]);
 		const size = el.clientWidth * 0.99;
 
 		let dimensions = {
@@ -164,6 +146,7 @@ const createChart = async () => {
 		//////////////////////////// colors ///////////////////////////////////////
 
 		const colorsType = ["#113655", "#113655", "#aaaaaa", "#5b8ea7"];
+
 		//////////////////////////// col var ///////////////////////////////////////
 
 		var dataType = _.chain(data)
@@ -171,10 +154,6 @@ const createChart = async () => {
 			.uniq()
 			.value();
 
-		// var xValue = _.chain(data)
-		// 	.map((d) => xAccessor(d))
-		// 	.uniq()
-		// 	.value();
 		var xValue = ["G7", "G20", "GGE", "LAS", "OAS", "OSCE", "SCO", "n/a"];
 
 		var yValue = _.chain(data)
@@ -183,19 +162,16 @@ const createChart = async () => {
 			.value()
 			.reverse();
 
-		// console.log(xValue);
 		console.log(yValue);
 
 		//////////////////////////// scales ///////////////////////////////////////
 
 		const xScale = d3
-			// .scaleOrdinal()
 			.scalePoint()
 			.domain(xValue)
 			.range([0, dimensions.boundedWidth]);
 
 		const yScale = d3
-			// .scaleOrdinal()
 			.scalePoint()
 			.domain(yValue)
 			.range([dimensions.boundedHeight, 0]);
@@ -204,8 +180,6 @@ const createChart = async () => {
 
 		//////////////////////////// axes /////////////////////////////////////////
 
-		// var formatAxis = d3.format(".4r");
-
 		const xAxisGenerator = d3
 			.axisBottom()
 			.scale(xScale)
@@ -213,18 +187,9 @@ const createChart = async () => {
 			.tickFormat((d) => d.substr(0, 4));
 
 		const xAxis = selectOrCreate("g", "xAxis", bounds)
-			// .style(
-			// 	"transform",
-			// 	`translate(${-radius * 1.5}px,${dimensions.boundedHeight}px)`
-			// )
 			.attr(
 				"transform",
-				"translate(" +
-					// dimensions.margin.left +
-					"0" +
-					"," +
-					(dimensions.boundedHeight + radius) +
-					")"
+				"translate(" + "0" + "," + (dimensions.boundedHeight + radius) + ")"
 			)
 			.transition()
 			.call(xAxisGenerator);
@@ -235,7 +200,6 @@ const createChart = async () => {
 			.tickSize(-dimensions.boundedWidth);
 
 		const yAxis = selectOrCreate("g", "yAxis", bounds).call(yAxisGenerator);
-		// 	.style("transform", `translate(0,${dimensions.boundedHeight}px)`);
 
 		//////////////////////////// plot /////////////////////////////////////////
 
@@ -308,50 +272,6 @@ const createChart = async () => {
 				d3.select(".tooltip").style("visibility", "hidden");
 				dots.transition().style("opacity", 1);
 			});
-
-			///////////////////////////////////////////////////////////////////////////
-			//////////////////////////// details //////////////////////////////////////
-			///////////////////////////////////////////////////////////////////////////
-
-			// var dataL = 0;
-			// var legendOffset = radius * 10;
-			// // var legendOffset = (dimensions.boundedWidth - 100) / dataType.length;
-
-			// var legend = selectOrCreate("g", "legend", bounds)
-			// 	.attr("width", dimensions.boundedWidth)
-			// 	.attr("height", radius * 2);
-
-			// var drawLegend = legend
-			// 	.selectAll(".legend")
-			// 	.data(dataType)
-			// 	.enter()
-			// 	.append("g")
-			// 	.attr("class", "legend")
-			// 	.attr("transform", function (d, i) {
-			// 		if (i === 0) {
-			// 			dataL = d.length + legendOffset;
-			// 			return "translate(0,0)";
-			// 		} else {
-			// 			var newdataL = dataL;
-			// 			dataL += d.length + legendOffset;
-			// 			return "translate(" + newdataL + ",0)";
-			// 		}
-			// 	});
-
-			// drawLegend
-			// 	.append("circle")
-			// 	.attr("cx", radius)
-			// 	.attr("cy", radius)
-			// 	.attr("r", radius / 2)
-			// 	.style("fill", (d, i) => colorsType[i]);
-
-			// drawLegend
-			// 	.append("text")
-			// 	.attr("x", radius + radius)
-			// 	.attr("y", radius * 1.5)
-			// 	.text((d) => d)
-			// 	.attr("class", "textselected")
-			// 	.style("text-anchor", "start");
 		};
 		dots(data);
 	};
